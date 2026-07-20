c3d4cf82e02540319b21882d84f012973126731b
sayedur468 <sayedur468@gmail.com>
2026-07-20 19:41:04 +0300

    Create products.js

---
+++ products.js
@@ -0,0 +1,154 @@
+/* ==========================================================
+   ShopTuch Premium E-Commerce Website
+   products.js
+========================================================== */
+
+"use strict";
+
+const products = [
+
+{
+    id:1,
+
+    title:"Wireless Bluetooth Earbuds",
+
+    category:"Electronics",
+
+    brand:"Baseus",
+
+    price:39.99,
+
+    oldPrice:59.99,
+
+    discount:33,
+
+    rating:4.9,
+
+    reviews:245,
+
+    stock:50,
+
+    sku:"ST1001",
+
+    badge:"Sale",
+
+    featured:true,
+
+    bestseller:true,
+
+    newArrival:false,
+
+    flashSale:true,
+
+    image:"assets/images/products/product-1.jpg"
+},
+
+{
+    id:2,
+
+    title:"Luxury Smart Watch",
+
+    category:"Watches",
+
+    brand:"LIGE",
+
+    price:79.99,
+
+    oldPrice:99.99,
+
+    discount:20,
+
+    rating:4.8,
+
+    reviews:186,
+
+    stock:35,
+
+    sku:"ST1002",
+
+    badge:"Hot",
+
+    featured:true,
+
+    bestseller:true,
+
+    newArrival:true,
+
+    flashSale:false,
+
+    image:"assets/images/products/product-2.jpg"
+},
+
+{
+    id:3,
+
+    title:"Men Premium Sneakers",
+
+    category:"Shoes",
+
+    brand:"Nike",
+
+    price:69.99,
+
+    oldPrice:89.99,
+
+    discount:22,
+
+    rating:4.7,
+
+    reviews:132,
+
+    stock:40,
+
+    sku:"ST1003",
+
+    badge:"New",
+
+    featured:true,
+
+    bestseller:false,
+
+    newArrival:true,
+
+    flashSale:false,
+
+    image:"assets/images/products/product-3.jpg"
+},
+
+{
+    id:4,
+
+    title:"Leather Hand Bag",
+
+    category:"Bags",
+
+    brand:"Charles & Keith",
+
+    price:59.99,
+
+    oldPrice:79.99,
+
+    discount:25,
+
+    rating:4.8,
+
+    reviews:214,
+
+    stock:22,
+
+    sku:"ST1004",
+
+    badge:"Sale",
+
+    featured:true,
+
+    bestseller:true,
+
+    newArrival:false,
+
+    flashSale:true,
+
+    image:"assets/images/products/product-4.jpg"
+}
+
+];
